a25304c767ea418401f7fef9e687d30cf488fd08
MinesesII <sonicyugi@gmail.com>
2016-01-31 14:27:21 +0100

    First commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    public int speed = 7;
+    private float move;
+    private bool facingRight = true;
+    private bool grounded = false;
+    private bool doubleJump = false;
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 400;
+    public GameObject shurikenPrefab;
+
+    private Animator animator;
+    private Rigidbody2D physic;
+
+    void Start(){
+
+        animator = GetComponent<Animator>();
+        physic = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+
+        if((grounded || !doubleJump) && Input.GetKeyDown(KeyCode.Space)){
+
+            animator.SetBool("Ground", false);
+            physic.AddForce(new Vector2(0, jumpForce));
+
+            if (!doubleJump && !grounded){
+
+                doubleJump = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E)){
+
+            Instantiate(shurikenPrefab, transform.position, Quaternion.identity);
+        }
+    }	
+
+	void FixedUpdate() {
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        animator.SetBool("Ground", grounded);
+
+        if (grounded){
+
+            doubleJump = false;
+        }
+        move = Input.GetAxis("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(move));
+        physic.velocity = new Vector2(move * speed, physic.velocity.y);
+
+        if(move > 0 && !facingRight){
+
+            Flip();
+        }
+        else if (move < 0 && facingRight){
+
+            Flip();
+        }
+    }
+
+    void Flip(){
+
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+}
---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+
+public class ProjectileScript : MonoBehaviour{
+
+    public int damage = 1;
+    public float speed = 20;
+    public bool isEnemyProjectile = false;
+
+    private Rigidbody2D physic;
+
+    ProjectileScript(){
+
+     /*   if (!toTheRight){
+
+            speed *= -1;
+        }*/
+        Debug.Log("test");
+    }
+
+    void Start(){
+
+        physic = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, 3);
+        //Debug.Log("test1");
+    }
+
+    void FixedUpdate(){
+
+        //var player = GameObject.FindWithTag("Player").GetComponent<>;
+        physic.velocity = new Vector2(speed, 0);
+        transform.Rotate(0, 0, -10);
+    }
+}
